ace7dc2a88139d54dafafdd548de2a563f0e02aa
Jose Illan <jose_illan@hotmail.com>
2020-03-29 19:02:05 +0200

    Fix no-image case using Pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,9 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 // Importar rutas
 import { ROUTES } from './app.routes';
 
+// Pipes 
+import { NoimagePipe } from './pipes/noimage.pipe';
+
 
 @NgModule({
   declarations: [
@@ -20,7 +23,8 @@ import { ROUTES } from './app.routes';
     HomeComponent,
     SearchComponent,
     ArtistaComponent,
-    NavbarComponent
+    NavbarComponent,
+    NoimagePipe
   ],
   imports: [
     BrowserModule,
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -12,21 +12,22 @@ export class SpotifyService {
     console.log('Spotify Service listo');
   }
 
-  getNewReleases() {
+  getQuery( query: string) {
+    const url = `https://api.spotify.com/v1/${ query }`;
     const headers = new HttpHeaders({
       'Authorization': 'Bearer BQCAnzHbS9DkY9JRlaFldqkjA930ynrVj7QaSOUDhPb8yHx4Qui7VniCKrDmO4TUNvO4jGJiF_IHe3sR5-c'
     });
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20',{ headers })
+    return this.http.get(url, { headers });
+  }
+
+  getNewReleases() {
+    return this.getQuery('browse/new-releases?limit=20')
                 .pipe( map( data => data['albums'].items));
   }
 
   getArtista( termino: string ){
-    const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQCAnzHbS9DkY9JRlaFldqkjA930ynrVj7QaSOUDhPb8yHx4Qui7VniCKrDmO4TUNvO4jGJiF_IHe3sR5-c'
-    });
-    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`,{ headers })
+    return this.getQuery(`search?q=${ termino }&type=artist&limit=15`)
                 .pipe( map( data => data['artists'].items));
-
   }
 
 
